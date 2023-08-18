f8fd420f4cfbbf0185f8c5798121c0dff1f9b6f2
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-18 22:47:21 +0300

    Wrote instructions

---
+++ Time.py
@@ -0,0 +1,5 @@
+# 1.Define a function, 
+# which will be given an hour (always in the range of 1 to 12, inclusive),
+# a minute (always in the range of 0 to 59, inclusive), 
+# and a period (either "am" or "pm") as input.
+# 2.Return a four-digit string that encodes that time in 24-hour time.
