f02949e381fc2d901251df5cbabddf1b3bdea9e6
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-18 22:31:24 +0300

    Wrote instructions

---
+++ Constant.py
@@ -0,0 +1,5 @@
+# 1. Given a lowercase string that has alphabetic characters only and no spaces, 
+# return the highest value of consonant substrings. 
+# 2. Consonants are any letters of the alphabet except "aeiou".
+# 3. We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
