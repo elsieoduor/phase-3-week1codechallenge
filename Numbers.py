1720636be6eee2f105344521b6e85160abe4e09b
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-20 20:10:12 +0300

    Completed challenge 2

--- Numbers.py
+++ Numbers.py
@@ -2,3 +2,20 @@
 # which takes three integers a, b, and c as arguments, 
 # and returns True if exactly two of of the three integers are positive numbers (greater than zero), 
 # and False - otherwise.
+
+def positive(a, b, c):
+    count = 0
+
+    if a > 0:
+        count += 1
+    if b > 0:
+        count += 1
+    if c > 0:
+        count += 1
+
+    return count == 2
+
+print(positive(4, 0, 2))
+
+
+
